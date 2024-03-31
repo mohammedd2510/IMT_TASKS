3280862f37732e10f8eedb6c4570f8bc64f7f778
mohamed <mohammad.osamaa25@gmail.com>
2024-03-31 22:53:57 +0200

    IR_CONTROL_RGB_TASK Added

---
+++ IR_CONTROL_RGB_TASK/Debug/Src/main.d
@@ -0,0 +1,29 @@
+Src/main.o: ../Src/main.c ../Inc/main.h ../Inc/HAL/IR_Receiver/IR_Lcfg.h \
+ ../Inc/HAL/IR_Receiver/IR_interface.h ../Inc/MCAL/GPIO/GPIO_interface.h \
+ ../Inc/MCAL/GPIO/../../LIB/BIT_MATH.h \
+ ../Inc/MCAL/GPIO/../../LIB/STD_TYPES.h \
+ ../Inc/MCAL/SYSTICK/SYSTICK_interface.h \
+ ../Inc/MCAL/SYSTICK/../../LIB/BIT_MATH.h \
+ ../Inc/MCAL/SYSTICK/../../LIB/STD_TYPES.h \
+ ../Inc/MCAL/EXTI/EXTI_interface.h ../Inc/MCAL/EXTI/../../LIB/STD_TYPES.h \
+ ../Inc/MCAL/EXTI/../../LIB/BIT_MATH.h ../Inc/HAL/RGB_LED/RGB_Lcfg.h \
+ ../Inc/HAL/RGB_LED/RGB_interface.h ../Inc/MCAL/RCC/RCC_interface.h \
+ ../Inc/MCAL/RCC/../../LIB/BIT_MATH.h \
+ ../Inc/MCAL/RCC/../../LIB/STD_TYPES.h
+../Inc/main.h:
+../Inc/HAL/IR_Receiver/IR_Lcfg.h:
+../Inc/HAL/IR_Receiver/IR_interface.h:
+../Inc/MCAL/GPIO/GPIO_interface.h:
+../Inc/MCAL/GPIO/../../LIB/BIT_MATH.h:
+../Inc/MCAL/GPIO/../../LIB/STD_TYPES.h:
+../Inc/MCAL/SYSTICK/SYSTICK_interface.h:
+../Inc/MCAL/SYSTICK/../../LIB/BIT_MATH.h:
+../Inc/MCAL/SYSTICK/../../LIB/STD_TYPES.h:
+../Inc/MCAL/EXTI/EXTI_interface.h:
+../Inc/MCAL/EXTI/../../LIB/STD_TYPES.h:
+../Inc/MCAL/EXTI/../../LIB/BIT_MATH.h:
+../Inc/HAL/RGB_LED/RGB_Lcfg.h:
+../Inc/HAL/RGB_LED/RGB_interface.h:
+../Inc/MCAL/RCC/RCC_interface.h:
+../Inc/MCAL/RCC/../../LIB/BIT_MATH.h:
+../Inc/MCAL/RCC/../../LIB/STD_TYPES.h:
